8f1afe987e4dcbcf058b71d72fc5a490afe3ef6a
AnGabSS <angelo.gs.santana@outlook.com>
2025-01-17 16:49:21 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const port = process.env.PORT ?? 8080;
+  await app.listen(port);
+  console.log(`Application is running on port: ${port}`);
+}
+bootstrap();
